0597efac52b5ae0a652e47fef9095db66bcb90a1
Ralph Cacho <cacho.rc@gmail.com>
2018-04-07 15:41:08 -0700

    Add functionality to delete a reminder.

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { addReminder } from '../actions';
+import { addReminder, deleteReminder } from '../actions';
 
 class App extends Component {
   constructor(props) {
@@ -14,6 +14,10 @@ class App extends Component {
     this.props.addReminder(this.state.text);
   }
 
+  deleteReminder(id) {
+    this.props.deleteReminder(id);
+  }
+
   renderReminders() {
     const { reminders } = this.props;
     return (
@@ -22,7 +26,12 @@ class App extends Component {
           reminders.map(reminder => {
             return (
               <li key={reminder.id} className="list-group-item">
-                <div>{reminder.text}</div>
+                <div className="list-item">{reminder.text}</div>
+                <div
+                  className="list-item delete-button"
+                  onClick={() => this.deleteReminder(reminder.id)}>
+                  &#x2715;
+                </div>
               </li>
             )
           })
@@ -64,4 +73,4 @@ function mapStateToProps(state) {
   }
 }
 
-export default connect(mapStateToProps, { addReminder })(App);
+export default connect(mapStateToProps, { addReminder, deleteReminder })(App);
